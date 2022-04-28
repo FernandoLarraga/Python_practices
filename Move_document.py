b7054d1249d84a8c431a81d29759bcc2cf7ccb0e
Fernando Larraga <fernandolarraga97@gmail.com>
2022-04-28 18:57:24 -0500

    first commmit

---
+++ Move_document.py
@@ -0,0 +1,8 @@
+import shutil
+
+original = r"C:\Users\ferna\Downloads\Carpeta2\Mountain.docx" 
+target = r"C:\Users\ferna\Downloads\Carpeta1\Mountain.docx"
+
+shutil.move(original, target)
+
+'https://datatofish.com/move-file-python/'
